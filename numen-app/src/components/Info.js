d60208046fdadc8aac662ea95766cf699e71395f
cristianrozzi <cristianrozzi07@gmail.com>
2022-03-23 20:21:24 -0300

    componente info

---
+++ numen-app/src/components/Info.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Info() {
+  return (
+    <div>Info</div>
+  )
+}
+import React from 'react'
+
+export default function Info() {
+  return (
+    <div>Info</div>
+  )
+}

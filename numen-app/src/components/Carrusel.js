4e386aa0419124cfe418bb58f9cb820c6bb65f77
maxiconstancio <maxiconstancio@gmail.com>
2022-04-10 17:37:50 -0300

    autoplay

--- numen-app/src/components/Carrusel.js
+++ numen-app/src/components/Carrusel.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import {Autoplay, Pagination, Navigation} from 'swiper'
 // Import Swiper styles
 import 'swiper/css';
+import "swiper/css/pagination";
+
 export default function Carrusel() {
   return (
    <>
     <Swiper
       spaceBetween={10}
       slidesPerView={1}
+      autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
       breakpoints={{
           320: {
               slidesPerView: 1,
@@ -23,8 +29,11 @@ export default function Carrusel() {
             spaceBetween: 10
         },
       }}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
     >
       <SwiperSlide> <img className="w-96" src="https://appletoolbox.com/wp-content/uploads/2021/12/MacBook-Pro-2021-iPad-Mini-iPhone-13-Pro-AirPods-3-Beats-Fit-Pro-iPad-Pro-Best-Apple-Products-3-scaled.jpg" alt="sdfa"/></SwiperSlide>
       <SwiperSlide><img  className="w-96" src="https://www.thecoldwire.com/wp-content/uploads/2021/06/Flat-lay-composition-with-Apple-MacBook-Air-Silver-iPad-mini-4-and-iPhone-6s-on-wooden-background.jpg" alt="img" /></SwiperSlide>

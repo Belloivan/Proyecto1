f89a01cd22d30e66532c5188cfd6d3d41a593129
Ivan Bello <ivanbello10@gmail.com>
2022-04-19 21:05:43 -0300

    colocacion del background

--- numen-app/src/components/Redessociales.js
+++ numen-app/src/components/Redessociales.js
@@ -1,13 +1,13 @@
 import React from "react";
-import imagenFondo from "../assets/img/imagenFondo.jpg";
+import imagenFondo from "../assets/img/imagenFondo.jpg"
 
 export default function Redessociales () {
            return (
-            <div className="relative flex justify-center px-0 py-16 m-0 mx-auto" style={{background: URL ({imagenFondo})}}>
+            <div className="relative flex justify-center px-0 py-16 m-0 mx-auto" style={{backgroundImage: `url(${imagenFondo})`}}  >
                     <div className="container flex flex-col flex-wrap justify-between p-10 "> 
-                        <p className="flex justify-center mt-4 font-mono text-4xl font-light leading-none text-gray-800 bg-yellow-500 ">Tell a friend</p>
-                        <div className="flex flex-wrap justify-center gap-3 mt-12 bg-pink-500 sm:gap-10 mx-36">
-                            <div className="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-800 rounded-full opacity-50 cursor-pointer hover:bg-gray-700">
+                        <p className="flex justify-center mt-4 font-mono text-4xl font-light leading-none text-gray-800 ">Tell a friend</p>
+                        <div className="flex flex-wrap justify-center gap-3 mt-12 sm:gap-10 mx-36">
+                            <div className="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-red-500 rounded-full cursor-pointer hover:bg-gray-700">
                                 <svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         fillRule="evenodd"
